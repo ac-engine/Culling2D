ce691cf1454ca4d7f610de95cf84b775bf19d356
c_plus_c <dtm.music.producer@gmail.com>
2014-07-20 19:42:03 +0900

    プロジェクト構成をさらに更新

--- Culling2DTest/main.cpp
+++ Culling2DTest/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "../Culling2D/Culling/culling2d.World.h"
+
 int main(void)
 {
 	std::cout << "Hello World." << std::endl;
 
-	culling2d::World world = culling2d::World(10, culling2d::RectF());
+	culling2d::World *world = new culling2d::World(5, culling2d::RectF());
 
+	delete world;
 	system("pause");
 }
